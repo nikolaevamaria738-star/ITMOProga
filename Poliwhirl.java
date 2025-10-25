5924559a6e13f7ff0659a8541dee926b30535b88
nikolaevamaria738-star <nikolaevamaria738@gmail.com>
2025-10-25 10:59:37 +0300

    Update Poliwhirl.java

--- Poliwhirl.java
+++ Poliwhirl.java
@@ -1,21 +1,16 @@
 package mypokemons;
 
-import ru.ifmo.se.pokemon.Pokemon;
-import ru.ifmo.se.pokemon.Type;
-import mymoves.poliwag.*;
 import mymoves.poliwhirl.*;
 
-public class Poliwhirl extends Pokemon{
+public class Poliwhirl extends Poliwag{
 	public Poliwhirl(String name, int level) {
 		super(name,level);
 		
-		setType(Type.WATER);
 		setStats(65, 65, 65, 50, 50, 90);
-		Psychic psychic = new Psychic(90,100);
-		Swagger swagger = new Swagger(0,85);
 		HydroPump hydropump = new HydroPump(110, 80);
 		
-		super.setMove(psychic, swagger, hydropump);
+		super.setMove(hydropump);
 		
 	}
+
 }
