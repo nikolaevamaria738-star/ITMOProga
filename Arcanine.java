8d05a66c48e69c102a7bb2c9178100e0fd4f1743
nikolaevamaria738-star <nikolaevamaria738@gmail.com>
2025-10-25 11:01:47 +0300

    Update Arcanine.java

--- Arcanine.java
+++ Arcanine.java
@@ -1,23 +1,17 @@
 package mypokemons;
 
-import ru.ifmo.se.pokemon.Pokemon;
-import ru.ifmo.se.pokemon.Type;
 import mymoves.arcanine.*;
-import mymoves.growlithe.*;
 
-public class Arcanine extends Pokemon{
+public class Arcanine extends Growlithe{
 	public Arcanine(String name, int level) {
 		super(name,level);
 		
-		setType(Type.FIRE);
 		setStats(90, 110, 80, 100, 80, 95);
-		WillOWisp willowisp = new WillOWisp(0,85);
-		DoubleTeam doubleteam = new DoubleTeam(0,0);
-		WildCharge wildcharge = new WildCharge(90,100);
 		ExtremeSpeed extremespeed = new ExtremeSpeed(80,100,5,+2);
 		
-		super.setMove(willowisp, doubleteam, wildcharge, extremespeed);
+		super.setMove(extremespeed);
 		
 	}
+
 
 }
